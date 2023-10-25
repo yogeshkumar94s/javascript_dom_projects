afeb008898ff5b70a5d9d21e8133f3d22ab6865b
Yogesh Kumar <yksainimbd@gmail.com>
2023-10-25 22:59:15 +0530

    Updated Random Password Generator

--- random_password/index.js
+++ random_password/index.js
@@ -1,20 +1,55 @@
-function generateRandomPassword(length) {
-  const charset =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?";
-  let password = "";
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset.charAt(randomIndex);
+let inputSlider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
+
+const passBox = document.getElementById("passBox");
+const lowercase = document.getElementById("lowercase");
+const uppercase = document.getElementById("uppercase");
+const numbers = document.getElementById("numbers");
+const symbols = document.getElementById("symbols");
+const copyIcon = document.getElementById("copyIcon");
+
+const genBtn = document.getElementById("genBtn");
+
+sliderValue.innerText = inputSlider.value;
+inputSlider.addEventListener("input", () => {
+  sliderValue.innerText = inputSlider.value;
+});
+
+genBtn.addEventListener("click", () => {
+  passBox.value = generatePassord();
+});
+
+const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerChars = "abcdefghijklmnopqrstuvwxyz";
+const allNumbers = "0123456789";
+const allSymbols = "@#$%&*";
+
+function generatePassord() {
+  let genPassword = "";
+  allChars = "";
+
+  allChars += lowercase.checked ? lowerChars : "";
+  allChars += uppercase.checked ? upperChars : "";
+  allChars += numbers.checked ? allNumbers : "";
+  allChars += symbols.checked ? allSymbols : "";
+
+  let i = 1;
+  while (i <= inputSlider.value) {
+    genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+    i++;
   }
-  return password;
+  return genPassword;
 }
 
-const generatePasswordButton = document.getElementById("generatePassword");
-const passwordDisplay = document.getElementById("passwordDisplay");
+copyIcon.addEventListener("click", () => {
+  if (passBox.value != "" || passBox.value.length >= 1) {
+    navigator.clipboard.writeText(passBox.value);
+    copyIcon.innerText = "check";
+    copyIcon.title = "Password Copied!";
 
-generatePasswordButton.addEventListener("click", () => {
-  const passwordLength = 12; // Change this to your desired password length
-  const newPassword = generateRandomPassword(passwordLength);
-  passwordDisplay.textContent = newPassword;
-  passwordDisplay.classList.remove("hidden");
+    setTimeout(() => {
+      copyIcon.innerHTML = "content_copy";
+      copyIcon.title = "";
+    }, 2000);
+  }
 });
